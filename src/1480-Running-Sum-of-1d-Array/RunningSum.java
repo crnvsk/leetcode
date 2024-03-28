c42923df59f93234b8196fa0eda232dc00ac7de3
João Pedro Alves Carneiro Valadão <107374203+crnvsk@users.noreply.github.com>
2024-03-28 00:57:39 -0300

    Update workspace rename java classes from now on, all codes will be executed via bash 2958 to do
    Signed-off-by: João Pedro Alves Carneiro Valadão <107374203+crnvsk@users.noreply.github.com>

---
+++ src/1480-Running-Sum-of-1d-Array/RunningSum.java
@@ -0,0 +1,29 @@
+public class RunningSum {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        Solution solution = new Solution();
+
+        int[] result = solution.runningSum(nums);
+        for (int num : result) {
+            System.out.print(num + " ");
+        };
+    }
+}
+/**
+ * Calculates the running sum of an integer array.
+ * 
+ * @param An integer array nums and an integer k.
+ * @return The length of the longest good subarray of nums.
+ */
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        int sum = 0;
+        for (int i=0; i < nums.length; i++){
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
+}
