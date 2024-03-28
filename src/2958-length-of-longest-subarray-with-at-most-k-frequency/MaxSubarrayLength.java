be9ec48e6ea0c09011424056ac4be66822737437
João Pedro Alves Carneiro Valadão <107374203+crnvsk@users.noreply.github.com>
2024-03-28 01:42:46 -0300

    Restored .java

---
+++ src/2958-length-of-longest-subarray-with-at-most-k-frequency/MaxSubarrayLength.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxSubarrayLength {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,2,3,1,2};
+        int k = 2;
+        
+        Solution solution = new Solution();
+    
+        System.out.println(solution.maxSubarrayLength(nums, k));
+
+    }
+}
+
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        Map<Integer, Integer> numDict = new HashMap<>();
+        int resultado = 0;
+        for (int num : nums){
+            
+        }
+        return resultado;
+    }
+}
